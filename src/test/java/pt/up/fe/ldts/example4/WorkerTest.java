a53dc1bdff546c9af0da086080b4cedcfa592925
Du <luispedro.2003@hotmail.com>
2022-12-09 17:21:59 +0000

    example4

--- src/test/java/pt/up/fe/ldts/example4/WorkerTest.java
+++ src/test/java/pt/up/fe/ldts/example4/WorkerTest.java
@@ -8,7 +8,7 @@ public class WorkerTest {
 
     @Test
     public void testWorker() {
-        Worker worker = new Worker("John Doe", "+1 222-22222", "john", "secret");
+        Supervisor worker = new Supervisor("John Doe", "+1 222-22222", "john", "secret");
         assertEquals("John Doe", worker.getName());
         assertEquals("+1 222-22222", worker.getPhone());
         assertFalse(worker.login("john", "wrong"));
@@ -19,8 +19,8 @@ public class WorkerTest {
     public void testSupervisor() {
         Supervisor supervisor = new Supervisor("John Doe", "+1 222-22222", "john", "secret");
 
-        Worker minion1 = new Worker("Minion 1", "+1 123-12345", "minion1", "1234");
-        Worker minion2 = new Worker("Minion 2", "+1 123-12345", "minion2", "1234");
+        Supervisor minion1 = new Supervisor("Minion 1", "+1 123-12345", "minion1", "1234");
+        Supervisor minion2 = new Supervisor("Minion 2", "+1 123-12345", "minion2", "1234");
         supervisor.addSupervisee(minion1);
 
         assertEquals("John Doe", supervisor.getName());
